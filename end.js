aca5aa98566d8a612b630f0a1dbdeb8531513f69
Rahul Raveendran <rahullalu519@gmail.com>
2021-12-07 21:03:30 +0530

    linkes modified again

--- end.js
+++ end.js
@@ -28,7 +28,7 @@ saveHighScore = e =>{
 
     highScores.splice(5);
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign("/index.html")
+    window.location.assign("/Quiz_App/")
     console.log(highScores);
 
 }
